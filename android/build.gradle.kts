517ccada249b75f5b91b8e9e1bb69576388c3566
uzu09811 <168120233+uzu09811@users.noreply.github.com>
2024-11-15 19:53:10 +0530

    fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("com.android.library") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 buildscript {
