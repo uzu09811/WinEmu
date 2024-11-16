6e46f0fbe78f8b67f21c1e79105ff537ace3a857
uzu09811 <168120233+uzu09811@users.noreply.github.com>
2024-11-16 20:26:41 +0530

    Fix expired target sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,6 +62,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    lintOptions {
+        disable("ExpiredTargetSdkVersion") // Disable the lint check for expired target SDK
+    }
 }
 
 dependencies {
