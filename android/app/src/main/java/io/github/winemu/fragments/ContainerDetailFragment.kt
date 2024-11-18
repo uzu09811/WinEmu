e76c40de3c7e32c0167b7faeb8715577e62c4925
uzu09811 <168120233+uzu09811@users.noreply.github.com>
2024-11-19 00:39:36 +0530

    ContainerDetailFragment: Add args

--- android/app/src/main/java/io/github/winemu/fragments/ContainerDetailFragment.kt
+++ android/app/src/main/java/io/github/winemu/fragments/ContainerDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.preference.Preference
 import io.github.winemu.preferences.IntegerListPreference
 import androidx.preference.SwitchPreferenceCompat
+import com.win_lib.container.Container
 import io.github.winemu.R
 import io.github.winemu.MainActivity
 import io.github.winemu.fragments.BasePreferenceFragment
@@ -12,6 +13,9 @@ class ContainerDetailFragment : BasePreferenceFragment(), BasePreferenceFragment
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        arguments?.let {
+            container = it.getSerializable(ARG_CONTAINER) as Container
+        }
         // add configurations
         refresh()
     }
@@ -28,5 +32,15 @@ class ContainerDetailFragment : BasePreferenceFragment(), BasePreferenceFragment
 
     private fun refresh() {
         // add configurations
+    }
+
+    companion object {
+        fun newInstance(container: Container): ContainerDetailFragment {
+            val fragment = ContainerDetailFragment()
+            val args = Bundle()
+            args.putSerializable(ARG_CONTAINER, container)
+            fragment.arguments = args
+            return fragment
+        }
     }
 }
