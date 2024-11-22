449bac1956f38a68c7f3c0b16c7a77ba1d3e02e8
uzu09811 <168120233+uzu09811@users.noreply.github.com>
2024-11-22 19:43:58 +0530

    Update ContainerDetailFragment.kt

--- android/app/src/main/java/io/github/winemu/fragments/ContainerDetailFragment.kt
+++ android/app/src/main/java/io/github/winemu/fragments/ContainerDetailFragment.kt
@@ -9,10 +9,14 @@ import io.github.winemu.R
 import io.github.winemu.MainActivity
 import io.github.winemu.PreferenceActivity
 import io.github.winemu.fragments.BasePreferenceFragment
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class ContainerDetailFragment : BasePreferenceFragment(), BasePreferenceFragment.OnIntegerValueChangeListener {
 
     private lateinit var container: Container
+    private var data: JSONObject = JSONObject()
     
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -22,6 +26,7 @@ class ContainerDetailFragment : BasePreferenceFragment(), BasePreferenceFragment
         setIntegerValueChangeListener("graphics_driver", this)
         setIntegerValueChangeListener("audio_driver", this)
         refresh()
+	configureData()
     }
 
     override fun onValueChanged(key: String, newValue: Int) {
@@ -62,5 +67,32 @@ class ContainerDetailFragment : BasePreferenceFragment(), BasePreferenceFragment
 
     private fun configureToolbar() {
         (requireActivity() as PreferenceActivity).getSupportActionBar()?.title = "New Container" // TODO: use resources string so that can be transalated later
+    }
+
+    private fun configureData() {
+	data.put("name", name)
+        data.put("screenSize", Container.DEFAULT_SCREEN_SIZE)
+        data.put("envVars", Container.DEFAULT_ENV_VARS)
+        data.put("cpuList", Container.getFallbackCPUList())
+        data.put("cpuListWoW64", Container.getFallbackCPUListWoW64())
+        data.put("graphicsDriver", Container.DEFAULT_GRAPHICS_DRIVER)
+        data.put("dxwrapper", Container.DEFAULT_DXWRAPPER)
+        data.put("dxwrapperConfig", dxwrapperConfig)
+        data.put("audioDriver", Container.DEFAULT_AUDIO_DRIVER)
+        data.put("wincomponents", Container.DEFAULT_WINCOMPONENTS)
+        data.put("drives", Container.DEFAULT_DRIVES)
+        data.put("showFPS", true)
+        data.put("inputType", finalInputType)
+        data.put("wow64Mode", wow64Mode)
+        data.put("startupSelection", Container.STARTUP_SELECTION_ESSENTIAL)
+        data.put("box86Preset", box86Preset)
+        data.put("box64Preset", box64Preset)
+        data.put("desktopTheme", desktopTheme)
+        data.put("rcfileId", rcfileId)
+        data.put("midiSoundFont", midiSoundFont)
+        data.put("lc_all", lc_all)
+        data.put("primaryController", primaryController)
+        data.put("controllerMapping", controllerMapping)
+        data.put("wineVersion", WineInfo.MAIN_WINE_VERSION)
     }
 }
